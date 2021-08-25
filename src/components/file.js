5a9fc9ad746a40f0d60ecb879e3e248dc6c31e78
MiracleUFO <miracleufodiama@gmail.com>
2021-08-25 16:11:49 +0100

    fix: removed unused imports

--- src/components/file.js
+++ src/components/file.js
@@ -1,13 +1,10 @@
-import React,  {useState} from 'react';
+import React from 'react';
 
 import '../assets/css/file.css';
 
 
 const File = (props) => {
-
-    const [state, setState] = useState('');
     
-
     return (
         <li key={props.index} className="file" title={props.item}>{props.item}</li>
     )
